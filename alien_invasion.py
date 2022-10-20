b2090f579f5e92159b5dbd96f146291a85d54998
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2022-10-20 18:49:19 +0300

    Making code a bit cleaner

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,7 +5,7 @@ from ship import Ship
 from bullet import Bullet
 
 class AlienInvasion:
-    # Overall class to manage game assets and behavior.
+    ''' Overall class to manage game assets and behavior.'''
     def __init__(self):
         # Initialize the game, and create game resources.
         pygame.init()
@@ -16,6 +16,7 @@ class AlienInvasion:
         self.bullets = pygame.sprite.Group()
         # Set the background color.
         self.bg_color = (230, 230, 230)
+
     def run_game(self):
         # Start the main loop for the game.
         while True:
@@ -23,6 +24,7 @@ class AlienInvasion:
             self.ship.update()
             self._update_bullets()
             self._update_screen()
+
     def _check_events(self):
         # Respond to keypresses and mouse events.
         for event in pygame.event.get():
@@ -32,6 +34,7 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
     def _check_keydown_events(self, event):
         # Responds to keypresses.
         if event.key == pygame.K_RIGHT:
@@ -42,25 +45,30 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+
     def _check_keyup_events(self, event):
         # Responds to key releases.
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
     def _fire_bullet(self):
         # Create a new bullet and add it to the bullets group.
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
     def _update_bullets(self):
-        # Update position of bullets and get rid of old bullets.
+        ''' Update position of bullets and get rid of old bullets.'''
+
         # Update bullet positions.
         self.bullets.update()
         # Get rid of bullets that have dissapeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
     def _update_screen(self):
         # Update images on screen, and flip to the new screen.
         self.screen.fill(self.settings.bg_color)
@@ -69,6 +77,7 @@ class AlienInvasion:
             bullet.draw_bullet()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+        
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -1,7 +1,8 @@
 import pygame
 
 class Ship:
-    # A class to manage the ship.
+    ''' A class to manage the ship.'''
+
     def __init__(self, ai_game):
         # Initialize the ship and set its starting position.
         self.screen = ai_game.screen
@@ -17,8 +18,9 @@ class Ship:
         # Movement flags
         self.moving_right = False
         self.moving_left = False
+
     def update(self):
-        # Update the ship's position based on the movement flags.
+        ''' Update the ship's position based on the movement flags.'''
         # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
@@ -26,6 +28,7 @@ class Ship:
             self.x -= self.settings.ship_speed
         # Update rect object from self.x
         self.rect.x = self.x
+        
     def blitme(self):
         # Draw the ship at its current location.
         self.screen.blit(self.image, self.rect)

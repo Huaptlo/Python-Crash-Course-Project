53ff52f36411997a2f7d33d2ed3a779bae1f4014
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2022-10-25 19:09:02 +0300

    Alien added

--- bullet.py
+++ bullet.py
@@ -2,7 +2,8 @@ import pygame
 from pygame.sprite import Sprite
 
 class Bullet(Sprite):
-    # A class to manage bullets fired from the ship.
+    ''' A class to manage bullets fired from the ship.'''
+
     def __init__(self, ai_game):
         # Create a bullet object at the ship's current position.
         super().__init__()
@@ -14,3 +15,14 @@ class Bullet(Sprite):
         self.rect.midtop = ai_game.ship.rect.midtop
         # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
+
+    def update(self):
+        ''' Move bullet up the screen.'''
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        # Draw the bullet to the screen.
+        pygame.draw.rect(self.screen, self.color, self.rect)
